03d006aed68e8ac3aa0857a955a9f5a35f1ce155
Jagadeswar_robotics <pssjagadeswar22@gmail.com>
2026-05-20 03:03:18 +0530

    initial commit - SpiderX quadruped description package

---
+++ src/spiderx_description/launch/display.launch.py
@@ -0,0 +1,34 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    pkg = get_package_share_directory('spiderx_description')
+    urdf_file = os.path.join(pkg, 'urdf', 'spiderx.urdf.xacro')
+
+    robot_description = Command(['xacro ', urdf_file])
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        ),
+    ])
